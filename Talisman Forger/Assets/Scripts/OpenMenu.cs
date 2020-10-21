a424c511681ac586b8d50c937ee655007b273033
jtb1242 <jtb1242@mail.rit.edu>
2020-10-20 21:23:56 -0400

    Update UI in game scene

---
+++ Talisman Forger/Assets/Scripts/OpenMenu.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OpenMenu : MonoBehaviour {
+    public void RunMenuScene() {
+        SceneManager.LoadScene("Menu", LoadSceneMode.Single);    
+    }
+}
