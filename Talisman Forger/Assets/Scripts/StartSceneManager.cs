5b4ed0946fa171c3ddbddcaa2c3e67c00ba9e173
Ryan Rieger <rmr6894@rit.edu>
2020-10-22 14:24:54 -0400

    Fixed Start Scene Manager script

--- Talisman Forger/Assets/Scripts/StartSceneManager.cs
+++ Talisman Forger/Assets/Scripts/StartSceneManager.cs
@@ -9,37 +9,22 @@ public class StartSceneManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        DisplayText();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        RunMenuScene();
-        RunGameScene();
-    }
-    void DisplayText()
-    {
-        TextMesh title = GetComponent<TextMesh>();
-        title.text = "Talisman Forger";
-        title.color = Color.green;
-        title.alignment = TextAlignment.Center;
-        title.characterSize = 40;
+        
     }
 
     void RunMenuScene()
     {
-        if (Input.GetKeyDown(KeyCode.M))
-        {
-            SceneManager.LoadScene("Menu", LoadSceneMode.Single);
-        }
+        SceneManager.LoadScene("Menu", LoadSceneMode.Single);
     }
 
     void RunGameScene()
     {
-        if (Input.GetKeyDown(KeyCode.G))
-        {
-            SceneManager.LoadScene("MainScene", LoadSceneMode.Single);
-        }
+        SceneManager.LoadScene("MainScene", LoadSceneMode.Single);
     }
 }
